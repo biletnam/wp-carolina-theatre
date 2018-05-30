b37dce4bd1122c74212ac58f82b80f5041930064
Joey Carty <carty.joey@gmail.com>
2018-05-30 11:33:50 -0400

    Added To-Dos

--- wp-content/themes/carolina-theatre/404.php
+++ wp-content/themes/carolina-theatre/404.php
@@ -12,23 +12,16 @@
 
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section class="error-404 not-found">
+	<header class="page-header">
+		<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'carolinatheatre' ); ?></h1>
+	</header><!-- .page-header -->
+	<div class="page-content">
+		<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'carolinatheatre' ); ?></p>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'carolinatheatre' ); ?></h1>
-				</header><!-- .page-header -->
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'carolinatheatre' ); ?></p>
+		<?php get_search_form(); ?>
 
-					<?php get_search_form(); ?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
+	</div><!-- .page-content -->
+</section><!-- .error-404 -->
 
 <?php get_footer();
