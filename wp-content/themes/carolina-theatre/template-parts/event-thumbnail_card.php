06e3d4c0637429242e8f6ef1eadafaaf181fac73
Joey Carty <carty.joey@gmail.com>
2018-06-07 16:37:33 -0400

    Added more to Event Thumbs

--- wp-content/themes/carolina-theatre/template-parts/event-thumbnail_card.php
+++ wp-content/themes/carolina-theatre/template-parts/event-thumbnail_card.php
@@ -1,40 +1,33 @@
 <?php // TO-DO: Refactor taking into account the new event query
-// dates in YYYYMMDD format for easy comparing (ie: 20180130)
-$start_date = get_field('start_date');
-$end_date = get_field('end_date');
-$today = date("Ymd", strtotime('today'));
+
+/////// DATES 
+$start_date = get_field('start_date'); 		// YYYYMMDD format
+$end_date = get_field('end_date'); 				// YYYYMMDD format
+$today = date("Ymd", strtotime('today')); // YYYYMMDD format
 $showtime_closestToToday = $start_date; 
 
-// if no start date was given, set it as yesterday (so it doesn't show)
-if ($start_date == NULL) {
-	$start_date == $today-1;
-}
+if ($start_date == NULL) { $start_date = $today-1; } // if no start date is given, set it to yesterday (so event doesn't show)
+if($end_date == NULL) {	$end_date = $start_date; } // if a single day event, set end_date 
 
-// if a single day event, set end_date 
-if($end_date == NULL) {
-	$end_date = $start_date;
-}
-
-if ($showtimes != NULL) {
-  // only construct events if they are in the future
+// construct events' future dates
+// get most recent date for card's datebox & most recent time for card info
+if (have_rows('showtimes')) { 
   if ($end_date >= $today) {
 	  $event_dates = array();
 
-		if (have_rows('showtimes')) { 
-      while (have_rows('showtimes')) { the_row();
-    		$single_date = get_sub_field('date', false, false);
-    		if ($single_date >= $today) { // if the showtime is today or in the future,
-      		array_push($event_dates, $single_date);	// push date to array
-    		}
-       } // endwhile showtimes
-    } //endif showtimes 
+    while (have_rows('showtimes')) { the_row();
+  		$single_date = get_sub_field('date', false, false);
+  		if ($single_date >= $today) { // if the showtime is today or in the future,
+    		array_push($event_dates, $single_date);	// push date to array
+  		}
+     } // endwhile showtimes
     
     // get the date to show in the card as the date square
     $showtime_closestToToday = $event_dates[0]; 
   }
 }
 
-// assign correct classes to event depending if it's a film or live event
+/////// ASSIGN CLASS NAMES FOR EACH EVENT
 $class_names = [];
 if (get_post_type() == 'film') {
 	array_push($class_names, 'film'); 
@@ -46,15 +39,16 @@ if (get_post_type() == 'film') {
 	}  
 } 
 if (get_post_type() == 'event') {
+  array_push($class_names, 'event'); 
+
 	$event_categories = get_field('event_categories'); 
   if($event_categories){ 
 		foreach($event_categories as $event_category) {
 			array_push($class_names, $event_category);
 		}
-  	$class_names = get_field('event_categories'); 
   }
-  array_push($class_names, 'event'); 
 }
+
 // classes for associated (parent) Series and Festivals
 $associated_event = get_field('associated_event'); 
 if($associated_event){ 
@@ -84,7 +78,6 @@ for ($i = 0; $i < count($class_names); $i++) {
 
 
 <div class="card eventCard<?php echo ' ' . $class_string; ?>">
-   <span style="font-size: 8px;"><?php echo $class_string; ?></span>
    <a href="<?php echo get_page_link(get_the_id()); ?>">
     <div class="event__dateBox">
 			<span class="day"><?php echo date("j", strtotime($showtime_closestToToday)); ?></span>
@@ -108,14 +101,25 @@ for ($i = 0; $i < count($class_names); $i++) {
 		 	<img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" />
     </div>
     <div class="card__infoWrapper">
-        <p class="card__subtitle"><?php the_field('single_event_type');?></p>
+        <p class="card__subtitle">
+        	<?php if (get_post_type() == 'film') {
+        		echo 'film';
+        	} else if (get_post_type() == 'event') {
+      			$event_categories = get_field('event_categories'); 
+  					if($event_categories){ 
+  						echo join(", ", $event_categories);
+  					} else {
+	      			echo 'live event';
+	      		}
+      		} ?>
+        </p>
         <p class="card__title"><?php the_title();?></p>
         <div class="card__info">	
-        	<?php if (get_post_type() == "film") { ?>
+        	<?php if (get_post_type() == 'film'){ ?>
         		<?php $movie_info = get_field('film_information'); ?>
         		<p><i class="far fa-clock"></i><?php echo $movie_info["runtime"] . ' min'; ?></p>
 						<p><i class="far fa-film"></i><?php echo $movie_info["director"]; ?>, <?php echo $movie_info["release_year"]; ?></p>
-          <?php } else if (get_post_type() == "event") { ?>
+          <?php } else if (get_post_type() == 'event'){ ?>
           	<p><i class="far fa-clock"></i><?php echo $event_times[0]; ?> show</p>
             <p><i class="far fa-map-marker-alt"></i><?php the_field('location'); ?></p>
           <?php } ?>
