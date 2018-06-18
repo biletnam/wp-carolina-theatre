d075f99f07655f04fde99d9796c593fccf63c471
Joey Carty <carty.joey@gmail.com>
2018-06-18 16:47:31 -0400

    Update ticketing to hide button after past date

--- wp-content/themes/carolina-theatre/template-parts/event-ticket_buttons.php
+++ wp-content/themes/carolina-theatre/template-parts/event-ticket_buttons.php
@@ -3,36 +3,43 @@
 	$ticket_link = get_field('ticket_link'); 												// url
 	$tickets_onsaledate = get_field('tickets_onsaledate'); 					// 2018-06-06 20:55:09  |  Y-m-d H:i:s
 	$tickets_presaledate = get_field('tickets_presaledate'); 				// 2018-06-06 20:55:09  |  Y-m-d H:i:s
-	
+
+	$end_date = get_field('end_date', $featured_ID); 			
+  date_default_timezone_set('America/New_York');
+	$today = date("Ymd", strtotime('today'));
+
 	date_default_timezone_set('America/New_York');
 	$dateTime_now = date('Y-m-d H:i:s');
 
 	$event_soldout = get_field('event_soldout');
 ?>
-
-<?php if(!$event_soldout){ ?>
-	<?php 
-	if ($tickets_presaledate > $dateTime_now){ ?>
-		<a title="Presale tickets go on sale <?php echo date('l, F j', strtotime($tickets_presaledate)); ?> at <?php echo date('g:ia', strtotime($tickets_presaledate)); ?>" class="button disabled"><i class="far fa-ticket-alt"></i> Presale Begins <?php echo date('n/j', strtotime($tickets_presaledate)); ?></a>
-  	<p class="small"><i><strong>General Tickets</strong> <?php echo date('F j', strtotime($tickets_onsaledate)); ?> at <?php echo date('g:ia', strtotime($tickets_onsaledate)); ?></i></p>
-	<?php } 
-  else if($tickets_presaledate <= $dateTime_now && $tickets_presaledate != NULL) { ?>
-  	<a href="<?php echo $ticket_link; ?>" target="_blank" title="Purchase Presale Tickets to <?php the_title(); ?>" class="button"><i class="far fa-ticket-alt"></i> Presale Tickets</a>
-  	<p class="small"><i><strong>General Tickets</strong> <?php echo date('F j', strtotime($tickets_onsaledate)); ?> at <?php echo date('g:ia', strtotime($tickets_onsaledate)); ?></i></p>
-	<?php }
-	else if($tickets_onsaledate > $dateTime_now){ ?>
-		<a title="Tickets available <?php echo date('l, F j', strtotime($tickets_onsaledate)); ?> at <?php echo date('g:ia', strtotime($tickets_onsaledate)); ?>" class="button disabled"><i class="far fa-ticket-alt"></i> Tickets <?php echo date('n/j', strtotime($tickets_onsaledate)); ?></a>
-	<?php } 
-	else if($tickets_onsaledate <= $dateTime_now && $tickets_onsaledate != NULL){ ?>
-  	<a href="<?php echo $ticket_link; ?>" target="_blank" title="Purchase Tickets to <?php the_title(); ?>" class="button"><i class="far fa-ticket-alt"></i> Buy Tickets</a> 
-	<?php } 
-	else if($ticket_link != NULL && $tickets_onsaledate == NULL){ ?>
-  	<a href="<?php echo $ticket_link; ?>" target="_blank" title="Purchase Tickets to <?php the_title(); ?>" class="button"><i class="far fa-ticket-alt"></i> Buy Tickets</a> 
-	<?php }
-	else if($tickets) { ?> 
-  	<a target="_blank" title="Tickets To Be Announced" class="button disabled"><i class="far fa-ticket-alt"></i> Tickets TBA</a> 
-	<?php
-	} 
-} else { ?> 
-	<a title="Tickets are sold out." class="button disabled"><i class="far fa-ticket-alt"></i> Sold Out</a>
-<?php } // if event's sold out or not ?>
+<?php if($end_date >= $today){ ?>
+	<?php if(!$event_soldout){ ?>
+		<?php 
+		if ($tickets_presaledate > $dateTime_now){ ?>
+			<a title="Presale tickets go on sale <?php echo date('l, F j', strtotime($tickets_presaledate)); ?> at <?php echo date('g:ia', strtotime($tickets_presaledate)); ?>" class="button disabled"><i class="far fa-ticket-alt"></i> Presale Begins <?php echo date('n/j', strtotime($tickets_presaledate)); ?></a>
+	  	<p class="small"><i><strong>General Tickets</strong> <?php echo date('F j', strtotime($tickets_onsaledate)); ?> at <?php echo date('g:ia', strtotime($tickets_onsaledate)); ?></i></p>
+		<?php } 
+	  else if($tickets_presaledate <= $dateTime_now && $tickets_presaledate != NULL) { ?>
+	  	<a href="<?php echo $ticket_link; ?>" target="_blank" title="Purchase Presale Tickets to <?php the_title(); ?>" class="button"><i class="far fa-ticket-alt"></i> Presale Tickets</a>
+	  	<p class="small"><i><strong>General Tickets</strong> <?php echo date('F j', strtotime($tickets_onsaledate)); ?> at <?php echo date('g:ia', strtotime($tickets_onsaledate)); ?></i></p>
+		<?php }
+		else if($tickets_onsaledate > $dateTime_now){ ?>
+			<a title="Tickets available <?php echo date('l, F j', strtotime($tickets_onsaledate)); ?> at <?php echo date('g:ia', strtotime($tickets_onsaledate)); ?>" class="button disabled"><i class="far fa-ticket-alt"></i> Tickets <?php echo date('n/j', strtotime($tickets_onsaledate)); ?></a>
+		<?php } 
+		else if($tickets_onsaledate <= $dateTime_now && $tickets_onsaledate != NULL){ ?>
+	  	<a href="<?php echo $ticket_link; ?>" target="_blank" title="Purchase Tickets to <?php the_title(); ?>" class="button"><i class="far fa-ticket-alt"></i> Buy Tickets</a> 
+		<?php } 
+		else if($ticket_link != NULL && $tickets_onsaledate == NULL){ ?>
+	  	<a href="<?php echo $ticket_link; ?>" target="_blank" title="Purchase Tickets to <?php the_title(); ?>" class="button"><i class="far fa-ticket-alt"></i> Buy Tickets</a> 
+		<?php }
+		else if($tickets) { ?> 
+	  	<a target="_blank" title="Tickets To Be Announced" class="button disabled"><i class="far fa-ticket-alt"></i> Tickets TBA</a> 
+		<?php
+		} 
+	} else { ?> 
+		<a title="Tickets are sold out." class="button disabled"><i class="far fa-ticket-alt"></i> Sold Out</a>
+	<?php } // if event's sold out or not ?>
+<?php } else { ?>
+	<a title="This event has past. Tickets are no longer available." class="button disabled"><i class="far fa-ticket-alt"></i> Buy Tickets</a>
+<?php } // if event has past ?>
